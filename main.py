2c6911d1ab8e6edd6e6012ddc55076180db3f0c8
avorr <apvorobev@sberbank.ru>
2021-11-12 05:33:32 +0300

    test pipeline

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from os_passport import PassportsOS
 
 
 if __name__ == '__main__':
-    print(tuple(map(PassportsVM, portal_info)))
+    print(portal_info)
+    # print(tuple(map(PassportsVM, portal_info)))
     # all(map(PassportsOS, portal_info))
     print('!!!!!!!!!!!!!!!'*100)
