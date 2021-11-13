c3462c947c662ec85a22ee969b1986606e66fb8f
avorr <apvorobev@sberbank.ru>
2021-11-13 17:05:33 +0300

    test pipeline

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     # print(tuple(map(PassportsVM, portal_info)))
     # all(map(PassportsOS, portal_info))
     print('!!!!!!!!!!!!!!!'*100)
-    print(f'{cmdb_login} + login from jenkins')
-    print(f'{cmdb_password} + password from jenkins')
+    print(f'{cmdb_login[:1]} + login from jenkins')
+    print(f'{cmdb_password[:1]} + password from jenkins')
     # print(cmdb_password)
