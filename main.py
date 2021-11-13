f6e754654e00370abbfd0af01c7e78d2a004e21e
avorr <apvorobev@sberbank.ru>
2021-11-13 17:55:47 +0300

    test pipeline

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from os_passport import PassportsOS
 if __name__ == '__main__':
     # print(portal_info)
 
-    all(map(PassportsVM, portal_info))
-    # all(map(PassportsOS, portal_info))
+    # all(map(PassportsVM, portal_info))
+    all(map(PassportsOS, portal_info))
     # print('!!!!!!!!!!!!!!!'*100)
     # print(f'{cmdb_login[:-1]} + login from jenkins')
     # print(f'{cmdb_password[:-1]} + password from jenkins')
