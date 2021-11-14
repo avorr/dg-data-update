2b3bc6111e7eafcfdb86f5bd9f01abe9bc433641
avorr <apvorobev@sberbank.ru>
2021-11-14 16:17:49 +0300

    test pipeline

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     print(portal_info)
 
     # all(map(PassportsVM, portal_info))
-    all(map(PassportsOS, portal_info))
+    # all(map(PassportsOS, portal_info))
     visiableSetting()
