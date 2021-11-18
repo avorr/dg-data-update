7780c8f088c673845fb7ac666fbfb2594ebe9c39
avorr <apvorobev@sberbank.ru>
2021-11-18 23:57:09 +0300

    test pipe

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ if __name__ == '__main__':
     print(portal_info)
     print('######' * 100)
     all_objects = map(PassportsVM, portal_info)
-    all(map(PassportsOS, portal_info))
+    print('######' * 100)
+    print(type(all_objects))
+    print(len(all_objects))
+    print('######' * 100)
+    # all(map(PassportsOS, portal_info, all_objects))
+    tuple(PassportsOS(foo, all_objects) for foo in portal_info)
     if next(iter(portal_info)) == 'PD15':
         visiableSetting()
