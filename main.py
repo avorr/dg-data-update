91882985fe1a66e5f1e835edafaf8f74ad823361
avorr <apvorobev@sberbank.ru>
2021-12-01 18:37:34 +0300

    test pipeline

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from os_passport import PassportsOS
 from view_settings import visiableSetting
 
 if __name__ == '__main__':
+    print('#######' * 20)
     print(portal_info)
-    all_objects = max(map(PassportsVM, portal_info))
-    # all(map(PassportsOS, portal_info, all_objects))
-    tuple(PassportsOS(foo, all_objects) for foo in portal_info)
-    if next(iter(portal_info)) == 'PD15':
-        visiableSetting()
+    print('#######' * 20)
+    # all_objects = max(map(PassportsVM, portal_info))
+
+    ## all(map(PassportsOS, portal_info, all_objects))
+
+    # tuple(PassportsOS(foo, all_objects) for foo in portal_info)
+    # if next(iter(portal_info)) == 'PD15':
+    #     visiableSetting()
 
