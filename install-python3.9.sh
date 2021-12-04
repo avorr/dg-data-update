f55d86a7b41fb042d59330ecbeb713a221f3e0d1
avorr <apvorobev@sberbank.ru>
2021-12-05 02:35:45 +0300

    test pipeline

--- install-python3.9.sh
+++ install-python3.9.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
+
 {
-curl -k -O https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tar.xz
-tar -xf Python-3.9.9.tar.xz
-cd Python-3.9.9
-yum -y install gcc make && yum clean all
-./configure --enable-optimizations
-make altinstall || true
+
+    curl -k -O https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tar.xz
+    tar -xf Python-3.9.9.tar.xz
+    cd Python-3.9.9
+    yum -y install gcc make && yum clean all
+    ./configure --enable-optimizations
+    make altinstall || true
+
 } &> /dev/null
