572e5bd71b468af613ae739c9582b089379aaea1
avorr <apvorobev@sberbank.ru>
2021-12-05 22:59:24 +0300

    test pipeline

--- install-python3.9.sh
+++ install-python3.9.sh
@@ -5,8 +5,9 @@
     curl -k -O https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tar.xz
     tar -xf Python-3.9.9.tar.xz
     cd Python-3.9.9
-    yum -y install gcc make && yum clean all
+    yum -y install gcc make zlib-devel
     ./configure --enable-optimizations
     make altinstall || true
+    pip3.9 install requests pymongo
 
 } &> /dev/null
