3d00d2ed92c5a3a46afc8ac8bd31e90195539aae
avorr <apvorobev@sberbank.ru>
2021-12-06 23:17:19 +0300

    test pipeline

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#expect /opt/start-connect.exp $HOST $LOGIN $PASSWORD &
-#bash startvpn.sh &
-#sleep 10
-echo '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
-/usr/bin/python3 /opt/main.py
--- startvpn.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-expect /opt/start-connect.exp $HOST $FORTI_CRER_USR $FORTI_CRED_PSW
