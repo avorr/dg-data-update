799084b69b05f2aceee195f75381d644b9041b7a
avorr <apvorobev@sberbank.ru>
2021-12-07 01:12:11 +0300

    test pipeline

--- main.py
+++ main.py
@@ -10,12 +10,8 @@ if __name__ == '__main__':
     print(portal_info)
     print('########' * 30)
 
-    exit()
-
     all_objects = max(map(PassportsVM, portal_info))
-
     # all(map(PassportsOS, portal_info, all_objects))
-
     tuple(PassportsOS(foo, all_objects) for foo in portal_info)
     if next(iter(portal_info)) == 'PD15':
         visiableSetting()
