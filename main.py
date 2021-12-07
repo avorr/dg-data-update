5b2683fe40bd37d539bad5d84d69f61c550aed69
avorr <apvorobev@sberbank.ru>
2021-12-07 03:53:53 +0300

    test pipeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+
+
 from env import portal_info
 from vm_passport import PassportsVM
 from os_passport import PassportsOS
@@ -9,7 +11,7 @@ if __name__ == '__main__':
     print('########' * 30)
     print(portal_info)
     print('########' * 30)
-
+    # exit()
     all_objects = max(map(PassportsVM, portal_info))
     # all(map(PassportsOS, portal_info, all_objects))
     tuple(PassportsOS(foo, all_objects) for foo in portal_info)
