1d15b0941d48e842055d95a61fcb439cdf9720b5
avorr <apvorobev@sberbank.ru>
2021-12-09 21:43:45 +0300

    new brarnch for develop balancer with sni

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import time
 from env import portal_info
 from vm_passport import PassportsVM
 from os_passport import PassportsOS
-from view_settings import visiableSetting
 
 if __name__ == '__main__':
     print('########' * 30)
@@ -15,4 +14,5 @@ if __name__ == '__main__':
     # all(map(PassportsOS, portal_info, all_objects))
     tuple(PassportsOS(foo, all_objects) for foo in portal_info)
     if next(iter(portal_info)) == 'PD15':
+        from view_settings import visiableSetting
         visiableSetting()
--- prepare-image-pd20.sh
+++ prepare-image-pd20.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/bash
 
-apt-get update
-apt-get install curl -y
-curl https://filestore.fortinet.com/forticlient/downloads/FortiClientFullVPNInstaller_6.4.0.0851.deb -o FortiClientFullVPNInstaller_6.4.0.0851.deb
-apt-get install ./FortiClientFullVPNInstaller_6.4.0.0851.deb -y
+{
+  apt-get update
+  apt-get install curl -y
+  curl https://filestore.fortinet.com/forticlient/downloads/FortiClientFullVPNInstaller_6.4.0.0851.deb -o FortiClientFullVPNInstaller_6.4.0.0851.deb
+  apt-get install ./FortiClientFullVPNInstaller_6.4.0.0851.deb -y
 
-apt-get install -y -o APT::Install-Recommends=false -o APT::Install-Suggests=false \
-ca-certificates \
-iproute2 \
-expect \
-iputils-ping \
-screen
+  apt-get install -y -o APT::Install-Recommends=false -o APT::Install-Suggests=false \
+  ca-certificates \
+  iproute2 \
+  expect \
+  iputils-ping \
+  screen
+} &> /dev/null
 
 tar -xf python3.9.9.tar
 tar -C / -xapf python3.9.9.tar.gz
