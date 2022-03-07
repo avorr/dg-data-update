dbb3d2141cb9ea4b0c64d2141bb59cb72e39f8ef
avorr <apvorobev@sberbank.ru>
2022-03-07 23:15:28 +0300

    autoupdate apps-version-exporter PLAT-8012

--- pprb3Versions.py
+++ pprb3Versions.py
@@ -37,39 +37,39 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
     def create_object(version_info: dict, cmdb_token: str, type_id: str, author_id: int, method: str = 'POST',
                       template: bool = False) -> dict:
         if method == 'PUT':
-            # return cmdb_api(method, f'object/{version_info["public_id"]}', cmdb_token, version_info)
-            print(f'object/{version_info["public_id"]}')
+            return cmdb_api(method, f'object/{version_info["public_id"]}', cmdb_token, version_info)
+            # print(f'object/{version_info["public_id"]}')
 
         def get_version(version_info: dict) -> str:
-            if 'nginx_version' in version_info:
-                return version_info['nginx_version']
-            if 'pgsqlse_version' in version_info:
-                return version_info['pgsqlse_version']
-            if 'kafka_version' in version_info:
-                return version_info['kafka_version']
-            if 'wf_info' in version_info:
-                # print(next(iter(version_info['wf_info'])))
-                if next(iter(version_info['wf_info'])) == 'ERROR':
-                    return version_info['wf_info']['ERROR']
-                if next(iter(version_info['wf_info'])) == 'deployment':
-                    # print('\n'.join(version_info['wf_info']['deployment'].keys()), version_info['wf_info']['product-version'], version_info['wf_info']['release-version'], sep='\n')
-                    # print(version_info['wf_info']['product-version'], version_info['wf_info']['release-version'])
-                    # print(version_info['wf_info'])
-                    # print('\n'.join(('\n'.join(version_info['wf_info']['deployment'].keys()), version_info['wf_info']['product-version'], version_info['wf_info']['release-version'])))
-                    # print('##########')
-                    # return version_info['wf_info']['deployment']
-                    if not version_info['wf_info']['deployment']:
-                        return '\n'.join((version_info['wf_info']['product-version'],
-                                          version_info['wf_info']['release-version']))
+            # if 'nginx_version' in version_info:
+            #     return version_info['nginx_version']
+            # if 'pgsqlse_version' in version_info:
+            #     return version_info['pgsqlse_version']
+            # if 'kafka_version' in version_info:
+            #     return version_info['kafka_version']
+            # if 'version' in version_info:
+            # print(next(iter(version_info['version'])))
+            if next(iter(version_info['version'])) == 'ERROR':
+                return version_info['version']['ERROR']
+            if next(iter(version_info['version'])) == 'deployment':
+                # print('\n'.join(version_info['version']['deployment'].keys()), version_info['version']['product-version'], version_info['version']['release-version'], sep='\n')
+                # print(version_info['version']['product-version'], version_info['version']['release-version'])
+                # print(version_info['version'])
+                # print('\n'.join(('\n'.join(version_info['version']['deployment'].keys()), version_info['version']['product-version'], version_info['version']['release-version'])))
+                # print('##########')
+                # return version_info['version']['deployment']
+                if not version_info['version']['deployment']:
+                    return '\n'.join((version_info['version']['product-version'],
+                                      version_info['version']['release-version']))
 
-                    return '\n'.join(('\n'.join(version_info['wf_info']['deployment'].keys()),
-                                      version_info['wf_info']['product-version'],
-                                      version_info['wf_info']['release-version']))
+                return '\n'.join(('\n'.join(version_info['version']['deployment'].keys()),
+                                  version_info['version']['product-version'],
+                                  version_info['version']['release-version']))
 
-                return version_info['wf_info']
+            return version_info['version']
 
-            else:
-                return None
+            # else:
+            #     return None
 
         pprb3_object_template: dict = {
             "status": True,
@@ -97,6 +97,10 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
                 {
                     "name": "version",
                     "value": get_version(version_info)
+                },
+                {
+                    "name": "vm-id",
+                    "value": version_info['id']
                 }
             ]
         }
@@ -109,13 +113,12 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
     cmdb_token, user_id = get_dg_token()
     all_categories = get_all_jsons('categories', cmdb_token)
 
-    os_passports_category_id = category_id('pprb3-app-versions', 'Pprb3 App Versions', 'fas fa-server', cmdb_token,
-                                           all_categories)
+    os_passports_category_id = category_id('pprb3-app-versions', 'Pprb3 App Versions',
+                                           'fas fa-server', cmdb_token, all_categories)
 
     os_portal_category_id = \
         category_id(f'Pprb3-Versions-{portal_name}', f'Pprb3-Versions-{portal_name}', 'far fa-folder-open',
-                    cmdb_token, all_categories,
-                    os_passports_category_id['public_id'])
+                    cmdb_token, all_categories, os_passports_category_id['public_id'])
 
     all_pprb3_verions: dict = json.loads(requests.request("GET", portal_info[portal_name]['pprb3_versions']).content)
 
@@ -176,6 +179,11 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
                         "type": "text",
                         "name": "version",
                         "label": "version"
+                    },
+                    {
+                        "type": "text",
+                        "name": "vm-id",
+                        "label": "vm id"
                     }
                 ],
                 "active": True,
@@ -190,7 +198,8 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
                                 "vm-name",
                                 "local-ip",
                                 "tag",
-                                "version"
+                                "version",
+                                "vm-id"
                             ],
                             "type": "section",
                             "name": f"pprb3-versions-{portal_name}--{stand['project_id']}",
@@ -256,24 +265,15 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
 
             for version in stand['modules_version']:
                 create_pprb3_version_object = create_object(version, cmdb_token, create_type['result_id'], user_id)
-                # create_pprb3_version_object = create_object(version, cmdb_token, 190, user_id)
                 print('CREATE OBJECT', create_pprb3_version_object)
                 time.sleep(0.1)
 
-    # return
     all_objects: tuple = get_mongodb_objects('framework.objects')
     # from vm_passport import get_mongodb_objects
     # cmdb_projects = get_all_jsons('types', cmdb_token)
     # cmdb_projects: tuple = get_mongodb_objects('framework.types')
 
     all_pprb3_types = tuple(filter(lambda x: f'pprb3-versions-{portal_name}--' in x['name'], cmdb_projects))
-    # print(all_pprb3_types)
-
-    # for i in all_pprb3_types:
-    #     print(i['name'][21:])
-    #     print(i['label'])
-    # for pprb3_verions in all_pprb3_verions['info']:
-    #     print(pprb3_verions['project_id'])
 
     # for pprb3_type in all_pprb3_types:
     #     for pprb3_verions in all_pprb3_verions['info']:
@@ -282,101 +282,67 @@ def pprb3_versions(portal_name: str, all_objects: tuple = ()) -> None:
 
     # all_pprb3_types = reduce(lambda x, y: x + y, map(lambda foo: tuple(
     #     filter(lambda bar: f'pprb3-versions-{portal_name}--' in bar['name'], foo['results'])), cmdb_projects))
-    # print(all_pprb3_types)
 
-    # for i in all_pprb3_types:
-    #     print(i)
-
-    return
+    # return
     for pprb3_type in all_pprb3_types:
         for pprb3_verions in all_pprb3_verions['info']:
-            if pprb3_type['name'][21:] == pprb3_verions['project_id'] and pprb3_type['name'][21:] == \
-                    'fe6d283e-c96e-4b32-b1b7-72c004066f4e' and pprb3_verions['modules_version']:
-            # if pprb3_type['name'][21:] == pprb3_verions['project_id'] and pprb3_verions['modules_version'] != None:
+            if pprb3_type['name'][21:] == pprb3_verions['project_id'] and pprb3_verions['modules_version']:
+                # and pprb3_type['name'][21:] == 'fe6d283e-c96e-4b32-b1b7-72c004066f4e':
 
-                # print()
-                # print(pprb3_verions['modules_version'])
-                # print(pprb3_type['name'][21:])
-                # print(pprb3_verions['project_id'])
+                # if pprb3_type['name'][21:] == pprb3_verions['project_id'] and pprb3_verions['modules_version'] != None:
 
-                # if pprb3_type['label'] == pprb3_verions['cluster']:
                 dg_pprb3_objects = tuple(filter(lambda x: x['type_id'] == pprb3_type['public_id'], all_objects))
 
-                # for i in dg_pprb3_objects:
-                #     print(i)
-                # return
+                # foo = map(lambda x: '%s--%s' % (x['fields'][3]['value'], x['fields'][5]['value']), dg_pprb3_objects)
                 for pprb3_module in pprb3_verions['modules_version']:
-                    print(pprb3_module)
-
-
-                return
-                for ve_pprb3_object in all_pprb3_verions['info']:
-                    print(ve_pprb3_object)
-                    return
-                    if ve_pprb3_object['namespace'] not in map(lambda x: x.get('fields')[0]['value'], dg_pprb3_objects):
-                        print('NAMESPACE FOR CREATE', ve_pprb3_object['namespace'])
-                        # objects(ve_pprb3_object, cmdb_token, cmdb_cluster['public_id'], user_id, 'NAMESPACE')
-                        time.sleep(0.1)
-
-                    for cmdb_ns in dg_pprb3_objects:
-                        ns_template = objects(ve_pprb3_object, cmdb_token, cmdb_cluster['public_id'], user_id,
-                                              'NAMESPACE', template=True)
-                        if cmdb_ns['fields'][0]['value'] == ve_pprb3_object['namespace'] and cmdb_ns['fields'] != \
-                                ns_template['fields']:
-                            updateObjectTemplate: dict = {
-                                "type_id": cmdb_ns['type_id'],
-                                "status": True,
-                                "version": "1.0.1",
+                    for dg_object in dg_pprb3_objects:
+                        pprb3_object_tmp: dict = \
+                            create_object(pprb3_module, cmdb_token, pprb3_type['public_id'], user_id, template=True)
+                        if pprb3_module['id'] == dg_object['fields'][5]['value'] and \
+                                pprb3_module['tag'] == dg_object['fields'][3]['value'] and \
+                                pprb3_object_tmp['fields'] != dg_object['fields']:
+                            update_object_template: dict = {
+                                "type_id": dg_object['type_id'],
+                                "status": dg_object['status'],
+                                "version": dg_object['version'],
                                 "creation_time": {
-                                    "$date": int(datetime.datetime.timestamp(cmdb_ns['creation_time']) * 1000)
+                                    # "$date": int(datetime.datetime.timestamp(dg_object['creation_time']) * 1000)
+                                    "$date": int(time.mktime(dg_object['creation_time'].timetuple()) * 1000)
                                 },
-                                "author_id": cmdb_ns['author_id'],
+                                "author_id": dg_object['author_id'],
                                 "last_edit_time": {
                                     "$date": int(time.time() * 1000)
                                 },
                                 "editor_id": user_id,
-                                "active": True,
-                                "fields": ns_template['fields'],
-                                "public_id": cmdb_ns['public_id'],
-                                "views": 0,
+                                "active": dg_object["active"],
+                                "fields": pprb3_object_tmp['fields'],
+                                "public_id": dg_object['public_id'],
+                                "views": dg_object["views"],
                                 "comment": ""
                             }
+                            create_object(update_object_template, cmdb_token, pprb3_type['public_id'], user_id, 'PUT')
+                            print(f"UPDATE PPRB3 VERSION IN  {dg_object['type_id']} TYPE")
 
-                            time.sleep(0.1)
-                            print(f"UPDATE NAMESPACE in {cmdb_ns['type_id']}", ve_pprb3_object['namespace'])
-                            objects(updateObjectTemplate, cmdb_token, cmdb_cluster['public_id'], user_id, 'PUT')
+                        # if pprb3_module['id'] == dg_object['fields'][5]['value'] and \
+                        #         pprb3_module['tag'] == dg_object['fields'][3]['value']:
+                    if '%s--%s' % (pprb3_module['tag'], pprb3_module['id']) not in \
+                            map(lambda x: '%s--%s' % (x['fields'][3]['value'], x['fields'][5]['value']),
+                                dg_pprb3_objects):
+                        print('CREATE PPRB3 VERSION in %s' % pprb3_type['public_id'])
+                        create_object(pprb3_module, cmdb_token, pprb3_type['public_id'], user_id)
+                        time.sleep(0.1)
 
-                for cmdb_ns in dg_pprb3_objects:
-                    if cmdb_ns['fields'][0]['value'] not in map(lambda x: x['namespace'], cluster['info']):
-                        print('DELETE NAMESPACE', cmdb_ns['fields'][0]['value'])
-                        cmdb_api('DELETE', f"object/{cmdb_ns['public_id']}", cmdb_token)
+                for dg_object in dg_pprb3_objects:
+                    if '%s--%s' % (dg_object['fields'][3]['value'], dg_object['fields'][5]['value']) not in \
+                            map(lambda x: '%s--%s' % (x['tag'], x['id']), pprb3_verions['modules_version']):
+                        print('DELETE PPRB3 VERSION %s--%s' % (dg_object['fields'][3]['value'],
+                                                               dg_object['fields'][5]['value']))
+                        cmdb_api('DELETE', f"object/{dg_object['public_id']}", cmdb_token)
                         time.sleep(0.1)
 
 
 if __name__ == '__main__':
     pprb3_versions(next(iter(portal_info)))
-
-
-# for cmdb_cluster in allTypesLabels:
-#     for cluster in allLabels:
-#         if cmdb_cluster['label'] == cluster['cluster']:
-#             dg_pprb3_objects = tuple(filter(lambda x: x['type_id'] == cmdb_cluster['public_id'], all_objects))
-#
-#             for podInfo in cluster['labels']:
-#                 template = create_object(podInfo, cmdb_token, cmdb_cluster['public_id'], user_id, template=True)
-#                 if template['fields'] not in map(lambda x: x.get('fields'), dg_pprb3_objects):
-#                     createLabel = create_object(podInfo, cmdb_token, cmdb_cluster['public_id'], user_id)
-#                     print('CREATE LABEL <--->', template['fields'][1]['value'], createLabel)
-#                     time.sleep(0.1)
-#
-#             for cmdbLabel in dg_pprb3_objects:
-#                 if cmdbLabel['fields'][1]['value'] not in map(lambda x: x['name'], cluster['labels']):
-#                     print('DELETE LABEL <--->', cmdbLabel['fields'][1]['value'])
-#                     cmdb_api('DELETE', f"object/{cmdbLabel['public_id']}", cmdb_token)
-#                     time.sleep(0.1)
-
-
-#####################################
 
 
 def get_label(labels: dict, label: str) -> str:
