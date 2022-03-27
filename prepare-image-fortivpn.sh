ec8ebf59c200928b6e2bb42e2ae837d0191f7cd0
avorr <apvorobev@sberbank.ru>
2022-03-27 16:30:52 +0300

    DEBUG PLAT-10059

--- prepare-image-fortivpn.sh
+++ prepare-image-fortivpn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-{
+#{
   apt-get update
   apt-get install -y python3 \
                      python3-pip \
@@ -8,14 +8,9 @@
                      openfortivpn
 
   pip3 install --no-cache --upgrade requests pymongo
-} &> /dev/null
+#} &> /dev/null
 
 #  iputils-ping \
 #tar -xf python3.9.9.tar
 #tar -C / -xapf python3.9.9.tar.gz
 #tar -xf venv.tar.gz
-
-
-
-
-apt-get install -y python3 python3-pip screen openfortivpn
