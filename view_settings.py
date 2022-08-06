16dbecf6810245f2d611142605dd68dcd16858b5
avorr <apvorobev@sberbank.ru>
2022-08-06 15:24:58 +0300

    Add fulldns name in name vdc column. PLAT-14505

--- view_settings.py
+++ view_settings.py
@@ -736,6 +736,7 @@ def visible_settings() -> None:
         create_settings(cmdb_projects_version)
         create_settings(cmdb_projects_vdc)
         create_settings(cmdb_projects_release)
+        print("WORK!!!")
 
 
 if __name__ == "__main__":
