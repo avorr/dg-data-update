2abe014a878585ea689a30c8901119ba85a04124
Алексакдр Воробьев <apvorobev@sberbank.ru>
2022-12-02 03:01:52 +0300

    Add info from new PD15, PLAT-18613.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from app_versions import gtp_app_versions
 def main() -> None:
     # all_objects: tuple = PassportsVM(sys.argv[1])
     # visible_settings()
+    # PassportsK8s(sys.argv[1])
     # LabelsK8s(sys.argv[1])
     # return
 
