5de1f86336dce62fa6eb56bccc5cdb756eca8474
Александр Воробьев <apvorobev@sberbank.ru>
2023-03-19 21:48:48 +0300

    Add vm and k8s links for Autopassport. PLAT-20957, PLAT-18613, PLAT-17926

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ from app_versions import gtp_app_versions
 
 def main() -> None:
     # visible_settings()
-    all_objects: tuple = PassportsVM(sys.argv[1])
+    # all_objects: tuple = PassportsVM(sys.argv[1])
     # PassportsK8s(sys.argv[1], all_objects)
     # PassportsK8s(sys.argv[1])
     # LabelsK8s(sys.argv[1])
-    return
+    # return
 
     try:
         all_objects: tuple = PassportsVM(sys.argv[1])
